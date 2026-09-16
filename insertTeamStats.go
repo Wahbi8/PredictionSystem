48c35ce5d8894c7d61b95ad4c1c1a3b35d072642
Oussama_Wahbi <wahbi.oussama08@gmail.com>
2026-09-16 20:51:51 +0100

    update

--- insertTeamStats.go
+++ insertTeamStats.go
@@ -1,4 +1,4 @@
-package main 
+package main
 
 import (
 	"context"
@@ -8,9 +8,10 @@ import (
 
 	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
+	"golang.org/x/text/cases"
 )
 
-func insertTeamStats(statsType string, season string) {
+func insertTeamStats(statsType string, season string, stats TeamStats) {
 	err := godotenv.Load()
 	if err != nil {
 		panic(err)
@@ -27,4 +28,26 @@ func insertTeamStats(statsType string, season string) {
 	defer conn.Close(ctx)
 
 	cleanSeason := strings.TrimSuffix(season, ".csv")
+
+	// statsType := []string{"Attack speed", "Formation", "Game state", 
+	// 	"Result", "Shot zones", "Situation", "Timing"}
+
+	var tableName string
+
+	switch statsType {
+	case "Attack speed":
+		tableName = "team_stats_by_attack_speed"
+	case "Formation":
+		tableName = "team_stats_by_formation"
+	case "Game state":
+		tableName = "team_stats_by_game_state"
+	case "Result":
+		tableName = ""
+	case "Shot zones":
+		tableName = "team_stats_by_shot_location"
+	case "Situation":
+		tableName = ""
+	case "Timing":
+		tableName = "team_stats_by_period"
+	}
 }
